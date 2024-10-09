d23c07372c15888a1a98d36dfb054d64218b3a9d
officialchandan6563 <officialchandan6563@gmail.com>
2024-10-10 01:28:16 +0530

    personality development

---
+++ src/app/personality-development/page.js
@@ -0,0 +1,15 @@
+import PersonalityBanner from "@/components/sections/personality-development/personalityBanner"
+import PersonalityFaq from "@/components/sections/personality-development/personalityFaq"
+import PersonalityOvereveiw from "@/components/sections/personality-development/personality-overview"
+
+function PersonalityDevelopment() {
+  return (
+    <div>
+      <PersonalityBanner/>
+      <PersonalityOvereveiw/>
+      <PersonalityFaq/>
+    </div>
+  )
+}
+
+export default PersonalityDevelopment
