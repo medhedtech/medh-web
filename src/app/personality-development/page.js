236879971d4a0b562cfa48debb1cfe766769415d
forhadmia1 <forhad8613@gmail.com>
2024-10-10 12:20:39 +0600

    course ai done

--- src/app/personality-development/page.js
+++ src/app/personality-development/page.js
@@ -1,15 +1,16 @@
-import PersonalityBanner from "@/components/sections/personality-development/personalityBanner"
-import PersonalityFaq from "@/components/sections/personality-development/personalityFaq"
-import PersonalityOvereveiw from "@/components/sections/personality-development/personality-overview"
+import PersonalityBanner from '@/components/sections/personality-development/personalityBanner';
+import PersonalityFaq from '@/components/sections/personality-development/personalityFaq';
+import PersonalityOvereveiw from '@/components/sections/personality-development/personality-overview';
+import PageWrapper from '@/components/shared/wrappers/PageWrapper';
 
 function PersonalityDevelopment() {
   return (
-    <div>
-      <PersonalityBanner/>
-      <PersonalityOvereveiw/>
-      <PersonalityFaq/>
-    </div>
-  )
+    <PageWrapper>
+      <PersonalityBanner />
+      <PersonalityOvereveiw />
+      <PersonalityFaq />
+    </PageWrapper>
+  );
 }
 
-export default PersonalityDevelopment
+export default PersonalityDevelopment;
