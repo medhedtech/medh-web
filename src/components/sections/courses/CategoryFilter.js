5f1802cb4e2aa90f7992c815e31efea0774721e4
subash7492 <subashpandit7492@gmail.com>
2024-10-14 10:07:58 +0530

    i have worked on personalaty-development

--- src/components/sections/courses/CategoryFilter.js
+++ src/components/sections/courses/CategoryFilter.js
@@ -1,7 +1,7 @@
-const CategoryFilter = ({ categories, setSelectedCategory }) => {
+const CategoryFilter = ({ categories, setSelectedCategory, heading }) => {
   return (
-    <div className="w-full md:w-1/4 bg-white p-4 rounded-md shadow-md">
-      <h2 className="text-xl font-semibold mb-4">Category</h2>
+    <div className="w-full  bg-white p-4 rounded-md">
+      <h2 className="text-2xl font-semibold mb-4 text-black">{heading}</h2>
       <div className="flex flex-col">
         {categories.map((category) => (
           <label key={category} className="inline-flex items-center mb-2">
@@ -14,11 +14,11 @@ const CategoryFilter = ({ categories, setSelectedCategory }) => {
           </label>
         ))}
       </div>
-      <div className="flex items-center border-[#5F2DED] border-2 w-fit px-1 rounded-md mt-4">
+      {/* <div className="flex items-center border-[#5F2DED] border-2 w-fit px-1 rounded-md mt-4">
         <button className="ml-4 p-1 text-sm text-[#5F2DED]">
           Clear All Filters
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
