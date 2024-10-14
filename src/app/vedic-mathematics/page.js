4eb0a397315d1ba3cb1634087191700536c084fb
officialchandan6563 <officialchandan6563@gmail.com>
2024-10-14 14:14:39 +0530

    vedic-mathematic component

--- src/app/vedic-mathematics/page.js
+++ src/app/vedic-mathematics/page.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import PageWrapper from "@/components/shared/wrappers/PageWrapper";
+import VedicBanner from '@/components/sections/vedic-mathematics/vedicBanner'
+import VedicOverview from '@/components/sections/vedic-mathematics/vedicOverview';
+import VedicCource from '@/components/sections/vedic-mathematics/vedicCource';
+import VedicFaq from '@/components/sections/vedic-mathematics/vedicFaq';
+// import PersonalityFaq from "@/components/sections/personality-development/personalityFaq";
+// import PersonalityOvereveiw from "@/components/sections/personality-development/personality-overview";
+// import PageWrapper from "@/components/shared/wrappers/PageWrapper";
+// import PersonalityCourse from "@/components/sections/personality-development/personalityCourse";
 
 function VedicMathematics() {
   return (
-    <div>
-      hiiii
-    </div>
+    <PageWrapper>
+    <VedicBanner />
+    <VedicOverview />
+    <VedicCource />
+    <VedicFaq />
+  </PageWrapper>
   )
 }
 
