b2ba82b71836c3d18b9e966f1964449bc538aca5
subash7492 <subashpandit7492@gmail.com>
2024-10-14 17:34:08 +0530

    I have worked Course AI Page

---
+++ src/app/course-ai/page.js
@@ -0,0 +1,25 @@
+import CourseAiOverview from "@/components/sections/course-ai/courseai-overview";
+import CourseAiBanner from "@/components/sections/course-ai/courseaiBanner";
+import CourseAiCourseBanner from "@/components/sections/course-ai/courseAiCourseBanner";
+import CourseAiFaq from "@/components/sections/course-ai/courseAiFaq";
+import CourseAiRelatedCourses from "@/components/sections/course-ai/courseAiRelatedCourse";
+import CourseOptions from "@/components/sections/course-ai/courseOptions";
+import Registration from "@/components/sections/registrations/Registration";
+import PageWrapper from "@/components/shared/wrappers/PageWrapper";
+import React from "react";
+
+function CouseAi() {
+  return (
+    <PageWrapper>
+      <CourseAiBanner />
+      <CourseAiOverview />
+      <CourseOptions />
+      <Registration />
+      <CourseAiFaq />
+      <CourseAiCourseBanner />
+      <CourseAiRelatedCourses />
+    </PageWrapper>
+  );
+}
+
+export default CouseAi;
