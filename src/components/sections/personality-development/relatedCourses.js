d2e3e21ad709c9dc69c7940b71decebf5f9a55e0
subash7492 <subashpandit7492@gmail.com>
2024-10-14 13:00:44 +0530

    added

---
+++ src/components/sections/personality-development/relatedCourses.js
@@ -0,0 +1,46 @@
+import React from "react";
+import CardImg1 from "./../../../assets/images/personality/courseimg1.jpeg";
+import CardImg2 from "./../../../assets/images/personality/courseimg2.jpeg";
+import CardImg3 from "./../../../assets/images/personality/courseimg3.jpeg";
+import CourseCard from "../courses/CourseCard";
+
+function RelatedCourses() {
+  const courses = [
+    {
+      title: "Certificate in",
+      label: "Vedic Mathematics",
+      grade: "(Grade 5-6)",
+      duration: "9 Months Course",
+      image: CardImg1,
+    },
+    {
+      title: "Foundation Certificate in",
+      label: "Digital Marketing with Data Analytics",
+      duration: "4 Months Course",
+      image: CardImg2,
+    },
+    {
+      title: "Advanced Certificate in",
+      label: "AI & Data Science",
+      duration: "8 Months Course",
+      image: CardImg3,
+    },
+  ];
+
+  return (
+    <div className="w-full bg-white h-auto py-6 flex justify-center items-center flex-col">
+        <p className="text-[#727695] text-lg font-semibold text-center px-4
+        ">We look forward to welcoming you on board to acquire the essential skills and knowledge to craft your success story</p>
+      <h1 className="text-center text-[#5C6574] text-3xl font-bold py-5 ">
+        Related Courses
+      </h1>
+      <div className="w-[80%] h-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {courses.map((course, index) => (
+          <CourseCard key={index} course={course} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default RelatedCourses;
