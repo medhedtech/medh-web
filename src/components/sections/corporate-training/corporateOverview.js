118b0224e0fdfba44daa653cb5da6840c11c920c
officialchandan6563 <officialchandan6563@gmail.com>
2024-10-15 22:10:56 +0530

    added-component

--- src/components/sections/corporate-training/corporateOverview.js
+++ src/components/sections/corporate-training/corporateOverview.js
@@ -8,8 +8,6 @@ const data = {
         name: "Overview",
         content:
           "Vedic Mathematics Course offers a wide array of benefits, from simplifying complex calculations to enhancing mental agility and boosting confidence. The system’s versatility and applicability across various branches of mathematics make it a valuable tool for individuals preparing for competitive exams or seeking to improve their mathematical skills. ",
-        contents:
-          "With its emphasis on simplicity, speed, and universality, Vedic Mathematics has the potential to transform the way individuals perceive and engage with mathematics, making it an essential skill for personal and professional growth.",
       },
       {
         id: 2,
@@ -19,38 +17,39 @@ const data = {
       },
       {
         id: 3,
-        name: "Career Prospects",
+        name: "What You'll Gain",
         content:
           "Vedic Mathematics provides excellent career prospects for those aiming to excel in mathematics, teaching, tutoring, and competitive exams.",
       },
-      {
-        id: 4,
-        name: "Sutras and Sub sutras",
-        content:
-          "This section covers the fundamental principles (sutras) and detailed applications (sub-sutras) for effective problem-solving in mathematics.",
-      },
+      
     ],
     overview: {
       keyFeatures: [
         {
-          title: "Simplicity",
+          title: "Customized Training Courses:",
           description:
-            "Aims to simplify complex mathematical calculations through its unique techniques.",
+            "Acknowledging the unique characteristics of each company, our seasoned trainers collaborate closely to tailor training courses that effectively address your IT and other training requirements. From AI, Data Science & Analytics, Cybersecurity, Cloud computing to Digital Marketing, Personality Development, our dynamic training courses are meticulously crafted to align with your business objectives.",
         },
         {
-          title: "Speed",
+          title: "Expert Instructors:",
           description:
-            "Methods are designed to expedite calculations, making them helpful for mental math and quick problem-solving.",
+            " Our trainers are seasoned IT professionals with extensive industry experience. They bring real-world insights and practical knowledgeto the training sessions, ensuring that your employees receive top-notch instruction and guidance.",
         },
         {
-          title: "Versatility",
+          title: "Flexible Learning Options: We",
           description:
-            "Offers multiple approaches to solve a single problem, allowing users to choose the method that suits them best.",
+            "We recognize that every company operates within its own scheduling constraints. Hence, we offer flexible learning options,including on-site training, virtual classrooms, and self-paced e-learning modules. This allows your employees to conveniently access our training courses,balancing learning with their regular work responsibilities.",
         },
         {
-          title: "Universality",
+          title: "Cutting-Edge Curriculum: ",
           description:
-            "Applicable to various branches of mathematics, such as arithmetic, algebra, trigonometry, calculus, and more.",
+            "Our training curriculum is regularly updated to reflect the latest trends and advancements in the IT industry. By enrolling in ourcourses, your employees will be equipped with the most relevant and up-to-date skills, empowering them to contribute meaningfully to your company’s success.",
+        },
+        {
+          title: "Hands-On Practical Experience:  ",
+          description:
+            "Hands-On Practical Experience: We firmly believe that experiential learning is one of the most effective ways to grasp complex IT concepts. Our training advancements in the IT industry. By enrolling in ourcourses, your employees will be equipped with the most relevant and up-to-date skills, empowering them to contribute meaningfully to your company’s success.",
+            descript:"Reach out to us today to discuss your training needs and let us design a customized training plan that aligns with your goals and aspirations. Together,let’s embark on a journey of growth and success through our Corporate Training Solutions."
         },
       ],
     },
@@ -72,13 +71,11 @@ export default function CorporateOverview() {
       <div className="w-full md:w-[80%]">
         {/* Title */}
         <div className="flex items-center flex-col w-80% md:mb-20 mb-10 px-4">
-          <h1 className="text-[24px] leading-7 md:text-4xl font-bold md:mb-3 mb-2 text-[#41454F]">
-            Welcome to Medh's Vedic Mathematics Course
+          <h1 className="text-[24px] leading-7 md:text-4xl font-bold md:mb-3 mb-2 text-[#000000]">
+          Gain a competitive edge with Medh's Corporate Training Courses
           </h1>
           <p className="text-center md:text-[15px] text-[14px] leading-6 md:leading-7 md:w-[70%] text-[#727695]">
-            Our course is designed to simplify mathematical problems through
-            ancient techniques that can be applied to a variety of real-world
-            applications.
+          Our SMART (Specific, Measurable, Achievable, Relevant, and Time-bound), approach ensures that the training is catering to your unique organizational goals.
           </p>
         </div>
 
@@ -89,9 +86,9 @@ export default function CorporateOverview() {
               key={tab.id}
               className={`px-2 md:px-6 md:py-2 py-1 transition md:mb-0 mb-2 ${
                 activeTab === tab.id
-                  ? "bg-[#5C40FF] text-white font-semibold"
-                  : "bg-[#E6E6FA] text-[#5C40FF] border-2 border-[#5C40FF]"
-              } hover:bg-[#5C40FF] hover:text-white`}
+                  ? "bg-[#5F2DED] text-white font-semibold"
+                  : "bg-[#E6E6FA] text-[#5F2DED] border-2 border-[#5F2DED]"
+              } hover:bg-[#5F2DED] hover:text-white`}
               onClick={() => setActiveTab(tab.id)}
             >
               {tab.name}
@@ -101,55 +98,24 @@ export default function CorporateOverview() {
 
         {/* Content Rendering */}
         <section className="bg-white px-5 md:px-6 py-8 border-2 border-gray-300 text-lightGrey14">
-          <h1 className="text-[23px] font-bold text-[#5C40FF]">
+          <h1 className="text-[23px] font-bold text-[#5F2DED]">
             {activeContent.name}
           </h1>
-          <p className="mb-2 md:text-[15px] text-[14px]">
-            {activeContent.content}
-          </p>
-          <p className="mb-2 md:text-[15px] text-[14px]">
-            {activeContent.contents}
-          </p>
 
           {activeTab === 1 && (
             <>
-              {/* Transformative Learning Experience */}
-              <h2 className="text-[1.3rem] font-bold mb-3 tracking-wide">
-                Transformative Learning Experience
-              </h2>
-              <p className="mb-2 text-[14px]">
-                In today’s competitive world, the ability to solve problems
-                quickly is crucial, and that’s where Vedic Mathematics comes to
-                your rescue. Through interactive and easy-to-follow lessons and
-                practice exercises, you will learn powerful techniques that make
-                math easy and enjoyable, including:
-              </p>
-              <ul className="list-disc list-inside mb-4">
-                <li className="text-[14px]">
-                  <strong>Easy Tricks to Solve:</strong> Addition, Subtraction,
-                  Multiplication, and Division
-                </li>
-                <li className="text-[14px]">
-                  <strong>Fast & Accurate Calculations of:</strong> Square Root,
-                  Cube, Cube Root, HCF, LCM, and Algebra
-                </li>
-              </ul>
-              <p className="text-[14px] mb-6">
-                And lots of other lessons that help you fall in love with
-                Mathematics.
-              </p>
+            
 
               {/* Key Features */}
-              <h2 className="text-[1.3rem] font-bold mb-4 tracking-wide">
-                Key Features of Vedic Mathematics:
-              </h2>
+              
               <ul className="list-none list-inside space-y-2 pb-2">
                 {data.overview.keyFeatures.map((feature, index) => (
                   <li key={index}>
                     <strong className="text-[1rem] font-bold tracking-wide">
                       {feature.title}:
                     </strong>{" "}
-                    {feature.description}
+                    {feature.description} <br />
+                    {feature.descript}
                   </li>
                 ))}
               </ul>
--- src/components/sections/corporate-training/courseBanner.js
+++ src/components/sections/corporate-training/courseBanner.js
@@ -2,15 +2,15 @@
 import React from 'react'
 import CourseBanner from "@/components/course-banner/courseBanner"; // Ensure this path is correct
 import CourseBannerImg from "../../../assets/images/personality/coursebannerimg.png";
+import DotIcon from "@/assets/images/corporate-training/dot-icon.svg"
 
-function corporateCourseBanner() {
+function CourceBanner() {
     const courses = [
         {
-          heading: "Ready to Elevate Your Presence?",
+          heading: "Let’s collaborate and discuss your training needs.",
           description:
-            "Enroll in MEDH's Personality Development Course and unlock your potential for a fulfilling personal and professional life.",
-          actionText: "Take Actions for Your Brighter Future!",
-          buttonText: "Enroll Now",
+            "Embark on a transformative journey towards success and unparalleled growth.",
+          buttonText: "Let’s Connect",
           imageUrl: CourseBannerImg, 
         },
       ];
@@ -25,7 +25,6 @@ function corporateCourseBanner() {
           key={index}
           heading={course.heading}
           description={course.description}
-          actionText={course.actionText}
           buttonText={course.buttonText}
           imageUrl={course.imageUrl}
           onButtonClick={() => handleEnrollClick(course)}
@@ -35,4 +34,4 @@ function corporateCourseBanner() {
   )
 }
 
-export default corporateCourseBanner
+export default CourceBanner
