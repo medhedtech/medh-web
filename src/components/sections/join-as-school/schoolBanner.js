1f326d747e08a9256fbf3d977424180b2172f7e1
officialchandan6563 <officialchandan6563@gmail.com>
2024-10-21 18:19:20 +0530

    added component

---
+++ src/components/sections/join-as-school/schoolBanner.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+import MainBanner from "@/components/course-banner/mainBanner";
+import Banner from "@/assets/images/join-educator/banner.png";
+import Cource from "@/assets/images/join-educator/educator-logo.svg";
+import Iso from "@/assets/images/vedic-mathematics/vedic-logo.svg";
+import LetsConnect from "@/assets/images/news-media/btn-vertical.svg";
+
+export default function SchoolBanner () {
+   
+
+  return (
+    <div>
+      <MainBanner
+        bannerImage={Banner}           
+        logoImage={Cource}               
+        isoImage={Iso}                 
+        heading="INNOVATE EDUCATION, EMPOWER STUDENTS."
+        subheading="Partner with Medh for Innovative Solutions"
+        description="Boost your students’ skill sets and enhance their future career prospects for greater success and opportunities."
+        buttonText="Explore More"
+        isoText="ISO CERTIFIED"
+        slogan="Medh Hain Toh Mumkin Hain!"
+        buttonImage={LetsConnect}
+      />
+    </div>
+  );
+}
+
+
+
+
