332123931379ac3158608f4d5e450871cd589215
officialchandan6563 <officialchandan6563@gmail.com>
2024-10-21 18:19:20 +0530

    added component

---
+++ src/components/sections/careers/careersCourseBanner.js
@@ -0,0 +1,42 @@
+"use client";
+import React from 'react'
+import CourseBanner from "@/components/course-banner/courseBanner"; // Ensure this path is correct
+import CourseBannerImg from "../../../assets/images/personality/coursebannerimg.png";
+
+import VerticalIcon from "@/assets/images/news-media/vertical-white.svg";
+
+function CareerCourceBanner() {
+  const courses = [
+    {
+      heading: "Ready to join our innovative team?",
+      description: "Explore our current job openings and apply today.",
+      buttonText: "Apply Now",
+      imageUrl: CourseBannerImg, 
+      buttonBgColor: "#F2277E", 
+      icon: VerticalIcon, 
+    },
+  ];
+
+  const handleEnrollClick = (course) => {
+    alert(`You clicked to enroll in: ${course.heading}`);
+  };
+
+return (
+<div>
+  {courses.map((course, index) => (
+    <CourseBanner
+      key={index}
+      heading={course.heading}
+      description={course.description}
+      buttonText={course.buttonText}
+      imageUrl={course.imageUrl}
+      onButtonClick={() => handleEnrollClick(course)}
+      buttonBgColor={course.buttonBgColor} 
+      icon={course.icon} 
+    />
+  ))}
+</div>
+);
+}
+
+export default CareerCourceBanner
---
+++ src/components/sections/join-as-school/schoolCourseBanner.js
@@ -0,0 +1,41 @@
+"use client";
+import React from 'react'
+import CourseBanner from "@/components/course-banner/courseBanner"; // Ensure this path is correct
+import CourseBannerImg from "../../../assets/images/personality/coursebannerimg.png";
+import DotIcon from "@/assets/images/corporate-training/dot-icon.svg"
+
+function SchoolCourceBanner() {
+  const courses = [
+    {
+      heading: "To create future-ready students through upskilling.",
+      buttonText: "Let’s Connect",
+      imageUrl: CourseBannerImg, 
+      buttonBgColor: "#F2277E", 
+      icon: DotIcon, 
+    },
+  ];
+
+  const handleEnrollClick = (course) => {
+    alert(`You clicked to enroll in: ${course.heading}`);
+  };
+
+return (
+<div>
+  {courses.map((course, index) => (
+    <CourseBanner
+      key={index}
+      heading={course.heading}
+      headings={course.headings}
+      description={course.description}
+      buttonText={course.buttonText}
+      imageUrl={course.imageUrl}
+      onButtonClick={() => handleEnrollClick(course)}
+      buttonBgColor={course.buttonBgColor} 
+      icon={course.icon} 
+    />
+  ))}
+</div>
+);
+}
+
+export default SchoolCourceBanner
