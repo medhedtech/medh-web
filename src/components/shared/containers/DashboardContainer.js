4fc2c4c2043d6d750696bfbedaee40faf65d7f86
Ritik Kumar Singh <ritikkrsingh1474@gmail.com>
2024-10-29 23:34:24 +0530

    student dashboard

--- src/components/shared/containers/DashboardContainer.js
+++ src/components/shared/containers/DashboardContainer.js
@@ -3,10 +3,10 @@ import SidebarDashboard from "../dashboards/SidebarDashboard";
 const DashboardContainer = ({ children }) => {
   return (
     <section>
-      <div className="container-fluid-2">
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-30px pt-30px pb-100px">
+      <div className="">
+        <div className="flex ">
           <SidebarDashboard />
-          <div className="lg:col-start-4 lg:col-span-9">{children}</div>
+          <div className="w-full">{children}</div>
         </div>
       </div>
     </section>
