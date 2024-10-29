dbccacf7e36c9d8b4380c3631a32f2417acc3b11
Ritik Kumar Singh <ritikkrsingh1474@gmail.com>
2024-10-29 23:34:24 +0530

    student dashboard

--- src/components/shared/dashboards/ItemsDashboard.js
+++ src/components/shared/dashboards/ItemsDashboard.js
@@ -4,10 +4,10 @@ const ItemsDashboard = ({ item }) => {
   const { title, items } = item;
   return (
     <>
-      <h5 className="text-sm leading-1 font-semibold uppercase text-contentColor dark:text-contentColor-dark bg-lightGrey5 dark:bg-whiteColor-dark p-10px pb-7px mt-5 mb-10px tracking-half">
+      {/* <h5 className="text-sm leading-1 font-semibold uppercase text-contentColor dark:text-contentColor-dark bg-lightGrey5 dark:bg-whiteColor-dark p-10px pb-7px mt-5 mb-10px tracking-half">
         {title}
-      </h5>
-      <ul>
+      </h5> */}
+      <ul className="w-full ">
         {items?.map((item1, idx) => (
           <ItemDashboard key={idx} item={item1} />
         ))}
