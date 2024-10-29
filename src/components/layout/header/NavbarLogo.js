71310d1f2d13ca57a9cb05e118f30028c780d51e
Ritik Kumar Singh <ritikkrsingh1474@gmail.com>
2024-10-29 23:34:24 +0530

    student dashboard

--- src/components/layout/header/NavbarLogo.js
+++ src/components/layout/header/NavbarLogo.js
@@ -6,12 +6,12 @@ const NavbarLogo = () => {
   return (
     <div className="lg:col-start-1 lg:col-span-2">
       <Link href="/" className="w-logo-sm lg:w-logo-lg ">
-        {/* <Image
+        <Image
           prioriy="fasle"
           src={logo1}
           alt="logo"
           className="w-full py-2 "
-        /> */}
+        />
       </Link>
     </div>
   );
---
+++ src/components/layout/main/dashboards/ProgressOverview.js
@@ -0,0 +1,43 @@
+import React from "react";
+import CourseCard from "./CourseCard";
+import progress1 from "@/assets/images/dashbord/progress1.png";
+import progress2 from "@/assets/images/dashbord/progress2.png";
+
+const ProgressOverview = () => {
+  const courses = [
+    {
+      id: 1,
+      title: "Web Development",
+      instructor: "John Doe",
+      progress: 40,
+      image: progress1,
+    },
+    {
+      id: 2,
+      title: "Java Full Stack",
+      instructor: "John Doe",
+      progress: 70,
+      image: progress2,
+    },
+  ];
+
+  return (
+    <div className="w-full py-6 px-10">
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-size-32 font-Open ">Progress Overview</h2>
+        <a href="#" className="text-green-500 hover:underline">
+          View All
+        </a>
+      </div>
+
+      {/* Display the courses */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {courses.map((course) => (
+          <CourseCard key={course.id} {...course} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProgressOverview;
