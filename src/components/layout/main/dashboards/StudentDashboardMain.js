547698c4ae4b4df9e745b1598b4735e716859288
Ritik Kumar Singh <ritikkrsingh1474@gmail.com>
2024-11-02 07:49:31 +0530

    Courses Details

--- src/components/layout/main/dashboards/StudentDashboardMain.js
+++ src/components/layout/main/dashboards/StudentDashboardMain.js
@@ -1,15 +1,17 @@
-import AdminFeedbacks from "@/components/sections/sub-section/dashboards/AdminFeedbacks";
-import CounterStudent from "@/components/sections/sub-section/dashboards/CounterStudent";
 import React from "react";
+import CounterStudent from "@/components/sections/sub-section/dashboards/CounterStudent";
 import ProgressOverview from "./ProgressOverview";
 import UpcomigClasses from "@/components/shared/dashboards/UpcomigClasses";
+import CourseDetailed from "@/app/course-detailed/page";
+import CourseDetails from "./CourseDetails";
 
 const StudentDashboardMain = () => {
   return (
     <>
-      <CounterStudent />
+      {/* <CounterStudent />
       <ProgressOverview />
-      <UpcomigClasses />
+      <UpcomigClasses /> */}
+      <CourseDetails />
       {/* <AdminFeedbacks /> */}
     </>
   );
