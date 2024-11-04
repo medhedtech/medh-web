84a49037402a7e4ba7136a1a11af73834b6f2f39
Ritik Kumar Singh <ritikkrsingh1474@gmail.com>
2024-11-04 16:32:49 +0530

    getting Bill

--- src/libs/getAllInstructors.js
+++ src/libs/getAllInstructors.js
@@ -5,12 +5,12 @@ import teacherImag3 from "@/assets/images/teacher/teacher__3.png";
 import teacherImag4 from "@/assets/images/teacher/teacher__4.png";
 import teacherImag5 from "@/assets/images/teacher/teacher__5.png";
 import teacherImag6 from "@/assets/images/teacher/teacher__6.png";
-import teacherImagLg1 from "@/assets/images/team/1.png";
-import teacherImagLg2 from "@/assets/images/team/2.png";
-import teacherImagLg3 from "@/assets/images/team/3.png";
-import teacherImagLg4 from "@/assets/images/team/4.png";
-import teacherImagLg5 from "@/assets/images/team/5.png";
-import teacherImagLg6 from "@/assets/images/team/6.png";
+import teacherImagLg1 from "@/assets/images/team/img1.png";
+import teacherImagLg2 from "@/assets/images/team/img2.png";
+import teacherImagLg3 from "@/assets/images/team/img3.png";
+import teacherImagLg4 from "@/assets/images/team/img4.png";
+import teacherImagLg5 from "@/assets/images/team/img5.png";
+import teacherImagLg6 from "@/assets/images/team/img6.png";
 const getAllInstructors = () => {
   const images = [
     teacherImag1,
