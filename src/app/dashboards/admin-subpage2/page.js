63a21ac3501c0fda86eacb978986f3754dd4da04
vipul103 <168165853+vipul103@users.noreply.github.com>
2024-11-05 10:33:24 +0530

    added few changes on admin dashboard

---
+++ src/app/dashboards/admin-subpage2/page.js
@@ -0,0 +1,27 @@
+import Omega from "@/components/layout/main/dashboards/Omega";
+import DashboardContainer from "@/components/shared/containers/DashboardContainer";
+import HeadingDashboard from "@/components/shared/headings/HeadingDashboard";
+import ThemeController from "@/components/shared/others/ThemeController";
+import DsahboardWrapper from "@/components/shared/wrappers/DsahboardWrapper";
+import PageWrapper from "@/components/shared/wrappers/PageWrapper";
+export const metadata = {
+  title: "Admin Reviews | Edurock - Education LMS Template",
+  description: "Admin Reviews | Edurock - Education LMS Template",
+};
+const Admin_Reviews = () => {
+  return (
+    <PageWrapper>
+      <main>
+        {/* <DsahboardWrapper> */}
+          <DashboardContainer>
+          <HeadingDashboard />
+            <Omega />
+          </DashboardContainer>
+        {/* </DsahboardWrapper> */}
+        <ThemeController />
+      </main>
+    </PageWrapper>
+  );
+};
+
+export default Admin_Reviews;
