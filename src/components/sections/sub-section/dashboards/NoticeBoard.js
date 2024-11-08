0da1fd51b5983487ec4f9ccd9020ee7d49c7d8a3
sandeep <s7895084627@gmail.com>
2024-11-08 15:46:06 +0530

    resolved few bugs

--- src/components/sections/sub-section/dashboards/NoticeBoard.js
+++ src/components/sections/sub-section/dashboards/NoticeBoard.js
@@ -12,7 +12,7 @@ const NoticeBoard = () => {
   const images = [blogImage4, blogImage6, blogImage7, blogImage8, blogImage9];
   return (
     <div className="p-10px md:px-10 md:py-50px mb-30px bg-whiteColor dark:bg-whiteColor-dark shadow-accordion dark:shadow-accordion-dark rounded-5 max-h-137.5 overflow-auto">
-      <HeadingDashboard path={"/blogs"}>Notice Board</HeadingDashboard>
+      {/* <HeadingDashboard path={"/blogs"}>Notice Board</HeadingDashboard> */}
 
       {/* instrutor */}
       <ul>
