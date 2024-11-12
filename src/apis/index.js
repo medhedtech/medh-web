bac924f9c187c87eee4c216d9a0b70713eb752ae
sandeep <s7895084627@gmail.com>
2024-11-12 10:39:28 +0530

    Added small changes

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,4 +1,5 @@
-export const apiBaseUrl = 'http://localhost:81/api/v1';
+// export const apiBaseUrl = 'http://localhost:81/api/v1';
+export const apiBaseUrl = 'http://localhost:8080/api/v1';
 
 export const apiUrls = {
   register: '/auth/register',
