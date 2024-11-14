ab49d00a3c39a93b2801b09845d9beb495ba7ffc
sandeep <s7895084627@gmail.com>
2024-11-14 11:23:25 +0530

    Added small changes

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,20 @@
-// export const apiBaseUrl = 'http://localhost:81/api/v1';
-export const apiBaseUrl = 'http://localhost:8080/api/v1';
+export const apiBaseUrl = "https://medh-backend.vercel.app/api/v1";
+// export const apiBaseUrl = 'http://localhost:8080/api/v1';
 
 export const apiUrls = {
-  register: '/auth/register',
-  login: '/auth/login',
+  user: {
+    register: "/auth/register",
+    login: "/auth/login",
+    update: "/auth/update/:id",
+    delete: "/auth/delete/:id",
+    getDetailsbyId: "/auth/get/:id",
+    getAll: "/auth/get-all",
+  },
+  courses: {
+    getAllCourses: "/courses/get",
+    getCourseById: "/courses/get/:id",
+    createCourse: "/courses/create",
+    updateCourse: "/courses/update/:id",
+    deleteCourse: "/courses/delete/:id",
+  },
 };
