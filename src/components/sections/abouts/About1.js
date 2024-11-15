11330d428b0100b9bdee0e9af27c05b44d98988c
Harsh <harsh.chauhan@clingmultisolutions.org>
2024-11-15 17:04:29 +0530

    Updated

--- src/components/sections/abouts/About1.js
+++ src/components/sections/abouts/About1.js
@@ -5,20 +5,21 @@ import React, { useEffect, useState } from "react";
 import aidata from "@/assets/images/about/ai-data-science.png";
 import { apiUrls } from "@/apis";
 import usePostQuery from "@/hooks/postQuery.hook";
+import useGetQuery from "@/hooks/getQuery.hook";
 
 const About1 = () => {
   const [courses, setCourses] = useState([]);
-  const { postQuery } = usePostQuery();
+  const { getQuery } = useGetQuery();
   const [limit] = useState(4);
   const [page] = useState(1);
 
   useEffect(() => {
     const fetchCourses = () => {
-      postQuery({
-        url: apiUrls?.courses.getAllCoursesWithLimits(page, limit),
+      getQuery({
+        url: apiUrls?.courses?.getAllCoursesWithLimits(page, limit,'','',''),
         onSuccess: (res) => {
           console.log("Response is:", res);
-          setCourses(res.courses || []);
+          setCourses(res?.courses || []);
         },
         onFail: (err) => {
           console.error("Error fetching courses:", err);
@@ -42,14 +43,14 @@ const About1 = () => {
 
         {/* Check if courses are valid */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-          {courses.slice(0, 4).map((course) => (
-            <Link href={`/courses/${course._id}`} key={course._id}>
+          {courses?.slice(0, 4)?.map((course) => (
+            <Link href={`/courses/${course?._id}`} key={course?._id}>
               <div className="bg-white border dark:border-whitegrey shadow-2xl flex flex-col overflow-hidden hover:scale-105 transition-transform duration-300 h-full">
                 <div className="relative w-full h-48">
                   <Image
                     src={aidata}
                     // src={course.course_videos?.[0] || aidata}
-                    alt={course.course_title}
+                    alt={course?.course_title}
                     fill
                     objectFit="cover"
                     className="w-full h-full"
@@ -57,10 +58,10 @@ const About1 = () => {
                 </div>
                 <div className="py-4 px-6 flex-grow dark:bg-screen-dark">
                   <h3 className="text-lg font-bold text-[#5C6574] dark:text-whitegrey3">
-                    {course.course_title}
+                    {course?.course_title}
                   </h3>
                   <p className="text-sm text-gray-500">
-                    {course.course_category}
+                    {course?.course_category}
                   </p>
                 </div>
               </div>
