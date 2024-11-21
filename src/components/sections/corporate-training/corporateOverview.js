8d6bc6ea2d46688c2c91a4cc57a8eddb81298365
sandeep <s7895084627@gmail.com>
2024-11-21 14:22:43 +0530

    added small changes

--- src/components/sections/corporate-training/corporateOverview.js
+++ src/components/sections/corporate-training/corporateOverview.js
@@ -1,90 +1,229 @@
 "use client";
 
 import React, { useState } from "react";
+
 const data = {
   tabs: [
     {
       id: 1,
       name: "Overview",
-      content:
-        "Vedic Mathematics Course offers a wide array of benefits, from simplifying complex calculations to enhancing mental agility and boosting confidence. The system’s versatility and applicability across various branches of mathematics make it a valuable tool for individuals preparing for competitive exams or seeking to improve their mathematical skills. ",
+      content: (
+        <>
+          <ul className="list-none list-inside space-y-2 pb-2 dark:text-gray300">
+            {[
+              {
+                title: "Customized Training Courses",
+                description:
+                  "Acknowledging the unique characteristics of each company, our seasoned trainers collaborate closely to tailor training courses that effectively address your IT and other training requirements. From AI, Data Science & Analytics, Cybersecurity, Cloud computing to Digital Marketing, Personality Development, our dynamic training courses are meticulously crafted to align with your business objectives.",
+              },
+              {
+                title: "Expert Instructors",
+                description:
+                  "Our trainers are seasoned IT professionals with extensive industry experience. They bring real-world insights and practical knowledge to the training sessions, ensuring that your employees receive top-notch instruction and guidance.",
+              },
+              {
+                title: "Flexible Learning Options",
+                description:
+                  "We recognize that every company operates within its own scheduling constraints. Hence, we offer flexible learning options, including on-site training, virtual classrooms, and self-paced e-learning modules. This allows your employees to conveniently access our training courses, balancing learning with their regular work responsibilities.",
+              },
+              {
+                title: "Cutting-Edge Curriculum",
+                description:
+                  "Our training curriculum is regularly updated to reflect the latest trends and advancements in the IT industry. By enrolling in our courses, your employees will be equipped with the most relevant and up-to-date skills, empowering them to contribute meaningfully to your company’s success.",
+              },
+              {
+                title: "Hands-On Practical Experience",
+                description:
+                  "We firmly believe that experiential learning is one of the most effective ways to grasp complex IT concepts. Our training includes hands-on practical exercises and projects, enabling your employees to apply their newfound knowledge in a controlled environment.",
+              },
+            ].map((feature, index) => (
+              <li key={index}>
+                <strong className="text-[1rem] font-bold tracking-wide dark:text-gray50">
+                  {feature.title}:
+                </strong>{" "}
+                {feature.description}
+              </li>
+            ))}
+          </ul>
+          <p className="text-lightGrey14 mb-6 md:text-[15px] text-[14px] dark:text-gray300 ">
+            Reach out to us today to discuss your training needs and let us
+            design a customized training plan that aligns with your goals and
+            aspirations. Together, let&#39;s embark on a journey of growth and
+            success through our Corporate Training Solutions.
+          </p>
+        </>
+      ),
     },
     {
       id: 2,
       name: "Benefits",
-      content:
-        "Benefits of Vedic Mathematics include improved speed in calculations, better mental agility, and stronger problem-solving skills. It's highly beneficial for students preparing for competitive exams.",
+      content: (
+        <>
+          <ul className="list-none list-inside space-y-2 pb-2 dark:text-gray300">
+            {[
+              {
+                title: "Attracting Top Talent",
+                description:
+                  "Organizations offering comprehensive training and development opportunities attract top-tier candidates. A positive reputation for employee growth serves as a magnet for highly skilled professionals, reducing the need for frequent recruitment efforts.",
+              },
+              {
+                title: "Improved Employee Skills",
+                description:
+                  "Training courses serve to enrich employees knowledge and skills, equipping them to execute their roles with enhanced effectiveness. This leads to heightened workplace productivity and efficiency, contributing to overall organizational success.",
+              },
+              {
+                title: "Enhanced Performance",
+                description:
+                  "As employees acquire new skills and knowledge, their performance ascends, fostering greater confidence and capability. Consequently, job performance improves, resulting in the delivery of high-quality outputs.",
+              },
+              {
+                title: "Increased Engagement",
+                description:
+                  "Providing tailored training programs communicates a commitment to employee growth, elevating satisfaction and engagement levels. This, in turn, leads to higher retention rates and a more contented workforce.",
+              },
+              {
+                title: "Technological Advances",
+                description:
+                  "Corporate training initiatives ensure that employees stay abreast of technological trends, thus bolstering the organization’s competitiveness and enabling the effective utilization of new tools and technologies.",
+              },
+
+              {
+                title: "Addressing Skill Gaps",
+                description:
+                  "Identifying and bridging skill gaps within the workforce is a key outcome of training courses, ensuring that employees possess the expertise necessary to achieve organizational objectives.",
+              },
+              {
+                title: "Consistency in Processes",
+                description:
+                  "Corporate training promotes standardization across departments, fostering improved internal collaboration, and ensuring utmost efficiency in operations.",
+              },
+              {
+                title: "Succession Planning",
+                description:
+                  "Training programs aid in talent development and succession planning by identifying high-potential employees for future leadership roles, contributing to the continuity of organizational leadership.",
+              },
+              {
+                title: "Increased Innovation",
+                description:
+                  "A culture of innovation thrives through continuous learning, as employees continuously developing skills generate fresh ideas and innovative solutions to challenges.",
+              },
+
+              {
+                title: "Improved Reputation",
+                description:
+                  "Employers investing in employee growth are perceived as more attractive. A positive reputation serves to draw top talent, enhancing the company’s market position.",
+              },
+              {
+                title: "Building Corporate Culture",
+                description:
+                  "An impactful training program that aligns values and strategies strengthens corporate culture, demonstrating to employees their significance and value within the company.",
+              },
+            ].map((feature, index) => (
+              <li key={index}>
+                <strong className="text-[1rem] font-bold tracking-wide dark:text-gray50">
+                  {feature.title}:
+                </strong>{" "}
+                {feature.description}
+              </li>
+            ))}
+          </ul>
+        </>
+      ),
     },
+
     {
       id: 3,
       name: "What You'll Gain",
-      content:
-        "Vedic Mathematics provides excellent career prospects for those aiming to excel in mathematics, teaching, tutoring, and competitive exams.",
+      content: (
+        <>
+          <p className="text-lightGrey14 mb-6 md:text-[15px] text-[14px] dark:text-gray300 ">
+            By enrolling in MEDH&#39;s Corporate Training Programs for your
+            employees, your company stands to gain a multitude of benefits,
+            including:
+          </p>
+          <ul className="list-none list-inside space-y-2 pb-2 dark:text-gray300">
+            {[
+              {
+                title: "Increased Productivity",
+                description:
+                  "Empower your team with the latest tools and strategies, enabling them to work more efficiently and enhance their overall output. By honing their skills and knowledge, employees become better equipped to contribute to the company’s productivity and success.",
+              },
+              {
+                title: "Elevated Performance",
+                description:
+                  "Witness your employees evolve into high-performing professionals, ready to take on any challenge. Through our tailored training programs, your workforce will gain the necessary skills and confidence to excel in their roles, ultimately elevating the overall performance of your organization.",
+              },
+              {
+                title: "Talent Retention",
+                description:
+                  "Cultivate a culture of continuous learning and growth within your company, attracting and retaining top talent in your industry. Offering robust training opportunities communicates a commitment to employee development, making your organization an appealing and rewarding place to work.",
+              },
+              {
+                title: "Increased Cross-Functional Collaboration",
+                description:
+                  "Training encourages employees to acquire a broader understanding of various functions within the organization, promoting cross-functional collaboration and knowledge sharing.",
+              },
+              {
+                title: "Strengthened Leadership",
+                description:
+                  "Effective training programs can identify and nurture future leaders within the organization, fostering a pipeline of capable individuals who can step into leadership roles when needed.",
+              },
+              {
+                title: "Enhanced Adaptability",
+                description:
+                  "Training equips employees with the skills and knowledge to adapt to changing market dynamics, technological advancements, and evolving customer needs, enabling the organization to stay agile and responsive.",
+              },
+            ].map((feature, index) => (
+              <li key={index}>
+                <strong className="text-[1rem] font-bold tracking-wide dark:text-gray50">
+                  {feature.title}:
+                </strong>{" "}
+                {feature.description}
+              </li>
+            ))}
+          </ul>
+          <p className="text-lightGrey14 mb-6 md:text-[15px] text-[14px] dark:text-gray300 ">
+            By investing in MEDH&#39;s Corporate Training Programs, your company
+            can expect a more efficient and skilled workforce, heightened
+            employee performance, and a competitive edge in talent retention
+            within the industry.
+          </p>
+        </>
+      ),
     },
   ],
-  overview: {
-    keyFeatures: [
-      {
-        title: "Customized Training Courses",
-        description:
-          "Acknowledging the unique characteristics of each company, our seasoned trainers collaborate closely to tailor training courses that effectively address your IT and other training requirements. From AI, Data Science & Analytics, Cybersecurity, Cloud computing to Digital Marketing, Personality Development, our dynamic training courses are meticulously crafted to align with your business objectives.",
-      },
-      {
-        title: "Expert Instructors",
-        description:
-          " Our trainers are seasoned IT professionals with extensive industry experience. They bring real-world insights and practical knowledgeto the training sessions, ensuring that your employees receive top-notch instruction and guidance.",
-      },
-      {
-        title: "Flexible Learning Options",
-        description:
-          "We recognize that every company operates within its own scheduling constraints. Hence, we offer flexible learning options,including on-site training, virtual classrooms, and self-paced e-learning modules. This allows your employees to conveniently access our training courses,balancing learning with their regular work responsibilities.",
-      },
-      {
-        title: "Cutting-Edge Curriculum ",
-        description:
-          "Our training curriculum is regularly updated to reflect the latest trends and advancements in the IT industry. By enrolling in ourcourses, your employees will be equipped with the most relevant and up-to-date skills, empowering them to contribute meaningfully to your company’s success.",
-      },
-      {
-        title: "Hands-On Practical Experience  ",
-        description:
-          "Hands-On Practical Experience: We firmly believe that experiential learning is one of the most effective ways to grasp complex IT concepts. Our training advancements in the IT industry. By enrolling in ourcourses, your employees will be equipped with the most relevant and up-to-date skills, empowering them to contribute meaningfully to your company’s success.",
-        descript:
-          "Reach out to us today to discuss your training needs and let us design a customized training plan that aligns with your goals and aspirations. Together,let’s embark on a journey of growth and success through our Corporate Training Solutions.",
-      },
-    ],
-  },
 };
 
-export default function CorporateOverview() {
+const CorporateOverview = () => {
   const [activeTab, setActiveTab] = useState(1);
 
   const activeContent = data.tabs.find((tab) => tab.id === activeTab);
 
   return (
-    <div className="bg-white dark:bg-screen-dark h-auto pt-10 pb-6 w-full flex justify-center items-center px-1 md:px-0">
+    <div className="bg-white dark:bg-screen-dark h-auto py-10  w-full flex justify-center items-center">
       <div className="w-full md:w-[80%]">
-        {/* Title */}
         <div className="flex items-center flex-col w-80% md:mb-20 mb-10 px-4">
-          <h1 className="text-[24px] leading-7 md:text-4xl font-bold md:mb-3 mb-2 dark:text-white text-[#000000]">
+          <h1 className="text-[24px] leading-7 md:text-4xl font-bold md:mb-3 mb-2 text-[#41454F] dark:text-gray-50">
             Gain a competitive edge with Medh&#39;s Corporate Training Courses
           </h1>
-          <p className="text-center md:text-[15px] text-[14px] leading-6 md:leading-7 md:w-[70%] dark:text-gray300 text-[#727695]">
-            Our SMART (Specific, Measurable, Achievable, Relevant, and
+          <p className="text-center md:text-[15px] text-[14px] leading-6 md:leading-7 md:w-[70%] text-[#727695] dark:text-gray-300">
+            meticulously designed to align with your company&#39;s vision and
+            mission. Our SMART (Specific, Measurable, Achievable, Relevant, and
             Time-bound), approach ensures that the training is catering to your
             unique organizational goals.
           </p>
         </div>
 
         {/* Tabs */}
-        <div className="flex md:mx-0  mx-4 space-x-2 flex-wrap">
+        <div className="flex md:mx-0 mx-4 space-x-2 flex-wrap">
           {data.tabs.map((tab) => (
             <button
               key={tab.id}
-              className={`px-2 md:px-6 md:py-2 py-1 transition sm:mb-0 mb-1 ${
+              className={`px-1 md:px-6 md:py-2 py-1  transition sm:mb-0 mb-1 ${
                 activeTab === tab.id
-                  ? "bg-[#7ECA9D] text-white font-semibold"
-                  : "bg-[#E6E6FA]  text-[#7ECA9D] border-2 border-[#7ECA9D]"
-              } hover:bg-[#7ECA9D] hover:text-white`}
+                  ? "bg-primaryColor text-white font-semibold"
+                  : "bg-white text-primaryColor border border-primaryColor"
+              } hover:bg-primaryColor hover:text-white`}
               onClick={() => setActiveTab(tab.id)}
             >
               {tab.name}
@@ -93,30 +232,15 @@ export default function CorporateOverview() {
         </div>
 
         {/* Content Rendering */}
-        <section className="bg-white dark:bg-inherit px-2 mx-4 md:mx-0 md:px-6 py-8 border-2 border-gray-300 dark:border-gray600 text-lightGrey14">
-          <h1 className="text-[23px] font-bold text-[#7ECA9D] ">
+        <section className=" bg-white mx-4 md:mx-0 dark:bg-screen-dark px-2 md:px-6 py-8 border-2 border-gray-200 text-lightGrey14">
+          <h1 className="text-[23px] font-bold text-primaryColor dark:text-gray50">
             {activeContent.name}
           </h1>
-
-          {activeTab === 1 && (
-            <>
-              {/* Key Features */}
-
-              <ul className="list-none list-inside space-y-2 pb-2 dark:text-gray300">
-                {data.overview.keyFeatures.map((feature, index) => (
-                  <li key={index}>
-                    <strong className="text-[1rem] font-bold tracking-wide dark:text-gray50">
-                      {feature.title}:
-                    </strong>{" "}
-                    {feature.description} <br />
-                    {feature.descript}
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
+          <div className="mt-4">{activeContent.content}</div>
         </section>
       </div>
     </div>
   );
-}
+};
+
+export default CorporateOverview;
--- src/components/sections/membership/membershipOverview.js
+++ src/components/sections/membership/membershipOverview.js
@@ -6,66 +6,130 @@ const data = {
   tabs: [
     {
       id: 1,
-      name: "Overview",
-      heading: "MEDH Membership Overview", // Heading for the Overview content
-      keyFeatures: [
-        {
-          description:
-            "MEDH Membership is designed to provide members with unparalleled access to resources, support, and opportunities that drive educational and professional development. Our membership program is tailored to meet the diverse needs of our community, offering a range of benefits that cater to individuals, students, and educational institutions.",
-        },
-      ],
+      name: "MEDH Membership Overview",
+      content: (
+        <>
+          <p className="text-lightGrey14 mb-6 md:text-[15px] text-[14px] dark:text-gray300 ">
+            MEDH Membership is designed to provide members with unparalleled
+            access to resources, support, and opportunities that drive
+            educational and professional development. Our membership program is
+            tailored to meet the diverse needs of our community, offering a
+            range of benefits that cater to individuals, students, and
+            educational institutions. By joining MEDH Membership, you become
+            part of a forward-thinking community dedicated to innovation and
+            excellence in education.
+          </p>
+        </>
+      ),
     },
     {
       id: 2,
       name: "Silver Membership",
-      heading: "MEDH Silver Membership",
-      keyFeatures: [
-        {
-          description:
-            "Silver Membership provides access to basic resources, educational materials, and support for personal development.",
-        },
-        {
-          description:
-            "Access to a monthly newsletter and exclusive webinars tailored to entry-level members.",
-        },
-      ],
+      content: (
+        <>
+          <ul className="list-none list-inside space-y-2 pb-2 dark:text-gray300">
+            {[
+              {
+                title:
+                  "You have a choice to explore and learn any or all courses within a ‘Single-Course-Category’ of your preferences",
+              },
+              {
+                title: "Exclusive Content Access",
+                description:
+                  "Enjoy access to a curated library of exclusive educational content, including webinars, tutorials, and research papers.",
+              },
+              {
+                title: "Discounts on Courses",
+                description:
+                  "Receive up to 20% discount on all MEDH skill development courses including LIVE and Blended both, allowing you to learn more for less.",
+              },
+              {
+                title: "Community Engagement",
+                description:
+                  "Participate in members-only forums and discussion groups to connect with like-minded individuals and share knowledge.",
+              },
+              {
+                title: "Monthly Newsletter",
+                description:
+                  "Stay informed with our monthly newsletter featuring the latest updates, tips, and insights from industry experts.",
+              },
+            ].map((feature, index) => (
+              <li key={index}>
+                <strong className="text-[1rem] font-bold tracking-wide dark:text-gray50">
+                  {feature.title}:
+                </strong>{" "}
+                {feature.description}
+              </li>
+            ))}
+          </ul>
+        </>
+      ),
     },
+
     {
       id: 3,
       name: "Gold Membership",
-      heading: "MEDH Gold Membership",
-      keyFeatures: [
-        {
-          description:
-            "Gold Membership offers premium access to a wealth of advanced resources and direct mentorship opportunities.",
-        },
-        {
-          description:
-            "Includes all benefits of Silver Membership plus personalized coaching sessions.",
-        },
-      ],
+      content: (
+        <>
+        <p className="text-lightGrey14 font-bold md:text-[15px] text-[14px] dark:text-gray300 ">
+          You have a choice to explore and learn any or all programs within
+          any of the Three-Course-Categories of your preference.
+        </p>
+        <p className="text-lightGrey14 md:text-[15px] text-[14px] dark:text-gray300 my-4">
+          All Silver Membership Benefits plus additional perks.
+        </p>
+          <ul className="list-none list-inside space-y-2 pb-2 dark:text-gray300">
+            {[
+              {
+                title: "Premium Content Access",
+                description:
+                  "Gain access to advanced courses, special workshops, and exclusive events that are not available to the general public.",
+              },
+              {
+                title: "Personalized Support",
+                description:
+                  "Receive one-on-one mentoring sessions and personalized guidance from our team of experts to help you achieve your goals.",
+              },
+              {
+                title: "Early Access to New Courses",
+                description:
+                  "Be the first to access new courses and programs before they are released to the public.",
+              },
+              {
+                title: "Higher Discount Rates",
+                description:
+                  "Enjoy up to 30% discount on all Medh courses, including LIVE and Blended both, maximizing your learning opportunities.",
+              },
+            ].map((feature, index) => (
+              <li key={index}>
+                <strong className="text-[1rem] font-bold tracking-wide dark:text-gray50">
+                  {feature.title}:
+                </strong>{" "}
+                {feature.description}
+              </li>
+            ))}
+          </ul>
+        </>
+      ),
     },
   ],
 };
 
-export default function MembershipOverview() {
+const MembershipOverview = () => {
   const [activeTab, setActiveTab] = useState(1);
 
   const activeContent = data.tabs.find((tab) => tab.id === activeTab);
 
   return (
-    <div className="bg-white dark:bg-screen-dark h-auto pt-10 pb-6 w-full flex justify-center items-center px-1 md:px-0">
-      <div className="w-full lg:w-[80%]">
-        {/* Title */}
-        <div className="flex items-center flex-col w-80% md:mb-20 mb-10 px-4 ">
-          <h1 className="text-[24px] leading-7 md:text-4xl font-bold md:mb-1 mb-2 dark:text-white text-[#000000]">
-            Unlock Your Potential with MEDH Membership!
+    <div className="bg-white dark:bg-screen-dark h-auto py-10  w-full flex justify-center items-center">
+      <div className="w-full md:w-[80%]">
+        <div className="flex items-center flex-col w-80% md:mb-20 mb-10 px-4">
+          <h1 className="text-[24px] text-center leading-7 md:text-4xl font-bold md:mb-3 mb-2 text-[#41454F] dark:text-gray-50">
+            Unlock Your Potential with MEDH Membership! Become an expert in your
+            chosen field of interest by gaining comprehensive knowledge and
+            skills.
           </h1>
-          <p className="text-[#F6B335] text-[1rem]">
-            Become an expert in your chosen field of interest by gaining
-            comprehensive knowledge and skills.
-          </p>
-          <p className="text-center md:text-[15px] dark:text-gray300 text-[14px] leading-6 md:leading-7 md:w-[90%] text-[#727695] mt-3">
+          <p className="text-center md:text-[15px] text-[14px] leading-6 md:leading-7 md:w-[70%] text-[#727695] dark:text-gray-300">
             Welcome to MEDH Membership, where we empower individuals, students,
             and educational institutions with exclusive benefits that foster
             growth, learning, and success. Whether you are looking to enhance
@@ -76,15 +140,15 @@ export default function MembershipOverview() {
         </div>
 
         {/* Tabs */}
-        <div className="flex md:mx-0 mx-4  flex-wrap">
+        <div className="flex md:mx-0 mx-4 space-x-2 flex-wrap">
           {data.tabs.map((tab) => (
             <button
               key={tab.id}
-              className={`px-2 md:px-6 mr-2 md:py-2 py-1 transition sm:mb-0 mb-1 ${
+              className={`px-1 md:px-6 md:py-2 py-1  transition sm:mb-0 mb-1 ${
                 activeTab === tab.id
-                  ? "bg-[#7ECA9D] text-white font-semibold"
-                  : "bg-[#E6E6FA]  text-[#7ECA9D] border-2 border-[#7ECA9D]"
-              } hover:bg-[#7ECA9D] hover:text-white`}
+                  ? "bg-primaryColor text-white font-semibold"
+                  : "bg-white text-primaryColor border border-primaryColor"
+              } hover:bg-primaryColor hover:text-white`}
               onClick={() => setActiveTab(tab.id)}
             >
               {tab.name}
@@ -93,19 +157,15 @@ export default function MembershipOverview() {
         </div>
 
         {/* Content Rendering */}
-        <section className="bg-white dark:bg-screen-dark  px-5 md:px-6 py-8 border-2 border-gray-300 text-lightGrey14 lg:mx-0 mx-4">
-          <h1 className="text-[23px] font-bold text-[#7ECA9D]">
-            {activeContent.heading}
+        <section className=" bg-white mx-4 md:mx-0 dark:bg-screen-dark px-2 md:px-6 py-8 border-2 border-gray-200 text-lightGrey14">
+          <h1 className="text-[23px] font-bold text-primaryColor dark:text-gray50">
+            {activeContent.name}
           </h1>
-
-          {/* Render Key Features */}
-          <ul className="list-none list-inside space-y-2 dark:text-white pb-2">
-            {activeContent.keyFeatures.map((feature, index) => (
-              <li key={index}>{feature.description}</li>
-            ))}
-          </ul>
+          <div className="mt-4">{activeContent.content}</div>
         </section>
       </div>
     </div>
   );
-}
+};
+
+export default MembershipOverview;
