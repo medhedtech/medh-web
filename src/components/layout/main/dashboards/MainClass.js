ff99cee05cff418f83b4ae1390794a2339fafdf9
Ritik Kumar Singh <ritikkrsingh1474@gmail.com>
2024-12-03 09:09:56 +0530

    Instructor Classes

---
+++ src/components/layout/main/dashboards/MainClass.js
@@ -0,0 +1,12 @@
+import HeadingDashboard from "@/components/shared/headings/HeadingDashboard";
+import React from "react";
+
+const MainClass = () => {
+  return (
+    <div className="px-4">
+      <HeadingDashboard></HeadingDashboard>
+    </div>
+  );
+};
+
+export default MainClass;
