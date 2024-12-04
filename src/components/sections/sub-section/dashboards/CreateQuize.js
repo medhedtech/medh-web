4d57f50c2cf5f0dd0adef11bb250e0cc25887e93
Ritik Kumar Singh <ritikkrsingh1474@gmail.com>
2024-12-04 21:40:25 +0530

    Create quiz page

--- src/components/sections/sub-section/dashboards/CreateQuize.js
+++ src/components/sections/sub-section/dashboards/CreateQuize.js
@@ -1,4 +1,7 @@
-import React from "react";
+"use client";
+import DateIcon from "@/assets/images/icon/Date";
+import TimeIcon from "@/assets/images/icon/TimeIcon";
+import React, { useState } from "react";
 
 const quizzes = [
   {
@@ -24,45 +27,25 @@ const quizzes = [
 ];
 
 const QuizCard = ({ quiz }) => (
-  <div className="bg-white shadow-md rounded-lg p-4 border border-gray-200">
-    <h3 className="text-lg font-semibold text-gray-800">{quiz.title}</h3>
-    <p className="text-sm text-gray-600">Course: {quiz.course}</p>
-    <p className="text-sm text-gray-600">Subject: {quiz.subject}</p>
+  <div className="bg-white dark:bg-inherit shadow-md rounded-lg p-4 border border-gray-200">
+    <h3 className="text-lg font-semibold text-[#3C3C3C] dark:text-white">
+      {quiz.title}
+    </h3>
+    <p className="text-sm text-[#9A9A9A] w-4/5">Course: {quiz.course}</p>
+    <p className="text-sm text-[#9A9A9A]">Subject: {quiz.subject}</p>
     <div className="flex items-center text-sm text-gray-600 mt-2">
-      <div className="flex items-center mr-4">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-5 h-5 mr-1"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8.25 15.75v-7.5M15.75 15.75v-7.5M9.75 8.25h4.5M12 3.75v3.75M6 21.75h12m-7.5 0h3"
-          />
-        </svg>
+      <div className="flex items-center mr-4 gap-2">
+        <DateIcon />
         {quiz.date}
       </div>
-      <div className="flex items-center">
-        {/* <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-5 h-5 mr-1"
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6l3 3" />
-        </svg> */}
+      <div className="flex items-center gap-2">
+        <TimeIcon />
         {quiz.time}
       </div>
     </div>
-    <p className="text-sm text-gray-600 mt-2">Questions: {quiz.questions}</p>
+    <p className="text-sm text-[#9A9A9A] mt-2">Questions: {quiz.questions}</p>
     <div className="mt-4">
-      <p className="text-sm text-gray-600">
+      <p className="text-sm flex justify-between mx-2 text-gray-600">
         Passing Percentage:
         <span className="font-semibold text-[#7ECA9D] ml-1">
           {quiz.passingPercentage}%
@@ -75,26 +58,86 @@ const QuizCard = ({ quiz }) => (
         ></div>
       </div>
     </div>
-    <button className="mt-4 w-full px-4 py-2 bg-white border-[#7ECA9D] border hover:text-white text-[#7ECA9D] text-sm font-semibold rounded hover:bg-green-600 transition">
+    <button className="mt-4 w-full px-4 py-2 bg-white dark:bg-inherit dark:hover:bg-[#7ECA9D] border-[#7ECA9D] border hover:text-white text-[#7ECA9D] text-sm font-semibold rounded hover:bg-green-600 transition">
       View Questions
     </button>
   </div>
 );
 
+const CreateQuizPopup = ({ onClose }) => (
+  <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="bg-white  rounded-lg shadow-lg p-6 w-full max-w-lg">
+      <div className="flex justify-between mb-4">
+        <h2 className="text-xl font-semibold font-Open  text-[#434343]">
+          Quiz Specifications
+        </h2>
+        <button type="button" onClick={onClose}>
+          x
+        </button>
+      </div>
+      <form className="">
+        <div>
+          <p>Time</p>
+          <div className="flex gap-4 text-[#3C3C3C]">
+            <div className="flex gap-2">
+              <DateIcon /> 3-01-2023
+            </div>
+            <div className="flex gap-2 my-auto">
+              <TimeIcon /> 12:30 AM - 01:40 PM
+            </div>
+          </div>
+          <select className="mt-8 block w-full border rounded-md h-fit p-2.5 border-black shadow-sm focus:ring-green-500 focus:border-green-500 sm:text-sm">
+            <option>Select Course</option>
+            <option>B.Tech Specialization in Health Informatics</option>
+            <option>B.Tech in Computer Science</option>
+          </select>
+        </div>
+        <div>
+          <select className="mt-4 block w-full h-fit p-2.5 border-black border rounded-md shadow-sm focus:ring-green-500 focus:border-green-500 sm:text-sm">
+            <option>Select Subject</option>
+            <option>Network Engineering</option>
+            <option>Data Structures</option>
+          </select>
+        </div>
+        <div className="">
+          <select className="mt-4 block w-full h-fit p-2.5 border-black border rounded-md shadow-sm focus:ring-green-500 focus:border-green-500 sm:text-sm">
+            <option>Total Questions</option>
+            <option>10</option>
+            <option>20</option>
+          </select>
+        </div>
+
+        <div className="flex justify-end space-x-2 ">
+          <button
+            type="submit"
+            className="px-4 mt-6 py-2 bg-[#7ECA9D] text-white w-full rounded-full text-sm font-medium"
+          >
+            Create Quiz
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+);
+
 const CreateQuize = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
   return (
-    <div className="px-6 pb-12">
+    <div className="px-6 pb-12 m-4 dark:bg-inherit  bg-white rounded-lg">
       <div className="flex justify-between items-center pt-4 mb-6">
-        <h2 className="text-2xl font-semibold text-gray-900">Create Quiz</h2>
-        <a href="#" className="text-green-500 hover:underline">
-          View all
-        </a>
+        <h2 className="text-2xl font-semibold text-gray-900 dark:text-white">
+          Create Quiz
+        </h2>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {quizzes.map((quiz) => (
           <QuizCard key={quiz.id} quiz={quiz} />
         ))}
-        <div className="flex flex-col justify-center items-center border-2 border-dashed border-gray-300 rounded-lg p-4 text-green-500 cursor-pointer hover:bg-green-50 transition">
+        <div
+          className="flex flex-col justify-center items-center border-2 border-dashed border-gray-300 rounded-lg p-4 text-green-500 cursor-pointer hover:bg-green-50 transition"
+          onClick={() => setIsPopupOpen(true)}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -112,6 +155,7 @@ const CreateQuize = () => {
           <p className="mt-2 font-semibold">Create New Quiz</p>
         </div>
       </div>
+      {isPopupOpen && <CreateQuizPopup onClose={() => setIsPopupOpen(false)} />}
     </div>
   );
 };
