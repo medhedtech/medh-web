b6b4de6cc81648f7e4b520e3441ad8ba4c714810
sandeep <s7895084627@gmail.com>
2024-12-06 17:04:56 +0530

    added small changes

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,6 +1,6 @@
 // export const apiBaseUrl = "https://medh-backend.vercel.app/api/v1";  //vercel URL
-export const apiBaseUrl = "https://13.202.119.19.nip.io/api/v1"; //live instance URL
-// export const apiBaseUrl = "http://localhost:8080/api/v1"; // local URL
+// export const apiBaseUrl = "https://13.202.119.19.nip.io/api/v1"; //live instance URL
+export const apiBaseUrl = "http://localhost:8080/api/v1"; // local URL
 
 export const apiUrls = {
   user: {
@@ -37,6 +37,7 @@ export const apiUrls = {
     getCourseNames: "/courses/course-names",
     getEnrolledCoursesByStudentId: "/enroll/student",
     toggleCourseStatus: "/courses/toggle-status",
+    addRecordedVideos: "/courses/recorded-videos"
   },
   upload: {
     uploadImage: "/upload/uploadImage",
