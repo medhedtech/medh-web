e64fe7ffb11ef093e35d2e40d6b91d814d12e8c6
sandeep <s7895084627@gmail.com>
2024-12-09 11:22:17 +0530

    Added new images for header banner

--- src/components/sections/corporate-training/corporateBanner.js
+++ src/components/sections/corporate-training/corporateBanner.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import MainBanner from "@/components/course-banner/mainBanner";
-import Banner from "@/assets/images/corporate-training/banner.png";
-import Cource from "@/assets/images/corporate-training/banner-logo.svg";
+import Banner from "@/assets/Header-Images/Corporate/ai-with-data-science.png";
+import Cource from "@/assets/Header-Images/Corporate/close-up-people-learning-job.jpg";
 import Iso from "@/assets/images/vedic-mathematics/vedic-logo.svg";
 import LetsConnect from "@/assets/images/news-media/btn-vertical.svg";
 
