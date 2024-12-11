613dfabbc6d5e1b80d121d85fb0be4a8affecf6f
sandeep <s7895084627@gmail.com>
2024-12-11 20:38:36 +0530

    added many changes

--- src/app/dashboards/instructor-track/page.js
+++ src/app/dashboards/instructor-track/page.js
@@ -1,11 +1,18 @@
+import Instructor_Tracking_component from "@/components/layout/main/dashboards/Instructor_Track";
 import DashboardContainer from "@/components/shared/containers/DashboardContainer";
+import HeadingDashboard from "@/components/shared/headings/HeadingDashboard";
 import ThemeController from "@/components/shared/others/ThemeController";
 import React from "react";
 
 const InstructorTrack = () => {
   return (
     <main>
-      <DashboardContainer></DashboardContainer>
+      <DashboardContainer>
+        <div className="px-8">
+          <HeadingDashboard />
+        </div>
+        <Instructor_Tracking_component />
+      </DashboardContainer>
       <ThemeController />
     </main>
   );
