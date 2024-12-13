63d08005fa95932e9c5dbce15276e415dad25ab5
sandeep <s7895084627@gmail.com>
2024-12-13 13:53:38 +0530

    Added small changes on the sheet

--- src/components/shared/headings/HeadingLg.js
+++ src/components/shared/headings/HeadingLg.js
@@ -1,8 +1,9 @@
 const HeadingLg = ({ children, color }) => {
   return (
     <div>
-      <h1 className="text-[#7ECA9D] text-[38px] font-bold leading-none w-full"
-      >
+      {/* <h1 className="text-[#7ECA9D] text-[38px] font-bold leading-none w-full"
+      > */}
+      <h1 className="text-[#7ECA9D] text-[2.9rem] font-bold leading-tight tracking-normal w-full">
         {children}
       </h1>
     </div>
