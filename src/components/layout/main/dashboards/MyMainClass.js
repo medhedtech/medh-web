364eca6564740b10d1ee08f4490f6882b356d9af
sandeep <s7895084627@gmail.com>
2024-12-14 13:03:20 +0530

    added small changes

--- src/components/layout/main/dashboards/MyMainClass.js
+++ src/components/layout/main/dashboards/MyMainClass.js
@@ -1,38 +1,53 @@
+"use client";
 import Image from "next/image";
-import React from "react";
-import AiMl from "@/assets/images/courses/Ai&Ml.jpeg";
+import React, { useEffect, useState } from "react";
 import Reactimg from "@/assets/images/courses/React.jpeg";
-import Os from "@/assets/images/courses/os.jpeg";
-import JavaScript from "@/assets/images/courses/javaScript.jpeg";
-
-const classes = [
-  {
-    id: 1,
-    course_image: AiMl,
-    meet_title: " Communication Skills",
-    disc: "Lorem ipsum dolor sit amet consectetur. Gravida adipiscing eget odio vitae sapien egestas.",
-  },
-  {
-    id: 2,
-    course_image: Reactimg,
-    meet_title: " Communication Skills",
-    disc: "Lorem ipsum dolor sit amet consectetur. Gravida adipiscing eget odio vitae sapien egestas.",
-  },
-  {
-    id: 3,
-    course_image: Os,
-    meet_title: " Communication Skills",
-    disc: "Lorem ipsum dolor sit amet consectetur. Gravida adipiscing eget odio vitae sapien egestas.",
-  },
-  {
-    id: 4,
-    course_image: JavaScript,
-    meet_title: " Communication Skills",
-    disc: "Lorem ipsum dolor sit amet consectetur. Gravida adipiscing eget odio vitae sapien egestas.",
-  },
-];
+import Preloader from "@/components/shared/others/Preloader";
+import { apiUrls } from "@/apis";
+import useGetQuery from "@/hooks/getQuery.hook";
 
 const MyMainClass = () => {
+  const [classes, setClasses] = useState([]);
+  const [instructorId, setInstructorId] = useState("");
+  const { getQuery, loading } = useGetQuery();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedUserId = localStorage.getItem("userId");
+      if (storedUserId) {
+        setInstructorId(storedUserId);
+      } else {
+        console.error("No instructor ID found in localStorage");
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (instructorId) {
+      const fetchUpcomingClasses = () => {
+        getQuery({
+          url: `${apiUrls?.onlineMeeting?.getMeetingsByInstructorId}/${instructorId}`,
+          onSuccess: (res) => {
+            setClasses(res.meetings || []);
+          },
+          onFail: (err) => {
+            console.error("Error fetching upcoming classes:", err);
+          },
+        });
+      };
+
+      fetchUpcomingClasses();
+    }
+  }, [instructorId]);
+
+  const liveAndDemoClasses = classes.filter(
+    (classItem) => classItem?.meeting_tag === "main"
+  );
+
+  if (loading) {
+    return <Preloader />;
+  }
+
   return (
     <div className="px-10 pb-12">
       <div className="flex justify-between items-center  pt-4 mb-4">
@@ -47,24 +62,28 @@ const MyMainClass = () => {
         </a>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        {classes.map((classItem) => (
+        {liveAndDemoClasses.map((classItem) => (
           <div
             key={classItem.id}
             className="bg-white dark:border dark:text-white dark:bg-inherit shadow rounded-lg p-4"
           >
             <div className="rounded overflow-hidden">
               <Image
-                src={classItem.course_image}
-                alt={classItem.meet_title}
-                className="w-full h-40 object-cover"
+                src={classItem.courseDetails?.course_image || Reactimg}
+                alt={classItem.meet_title || "Class Image"}
+                className="w-full h-48 object-cover rounded-lg transform hover:scale-105 transition-all duration-300"
                 width={300}
-                height={150}
+                height={200}
               />
             </div>
-            <h3 className="mt-2 font-semibold dark:text-white  text-gray-800">
-              {classItem.meet_title}
+            <h3 className="mt-2 font-semibold leading-tight dark:text-white text-gray-800">
+              {`${
+                classItem?.courseDetails?.course_title || "No Course Title"
+              } (${classItem?.meet_title || "No Meeting Title"})`}
             </h3>
-            <p className="text-size-10 font-light ">{classItem.disc}</p>
+            <p className="text-size-10 font-light ">
+              {classItem?.courseDetails?.course_description}
+            </p>
           </div>
         ))}
       </div>
