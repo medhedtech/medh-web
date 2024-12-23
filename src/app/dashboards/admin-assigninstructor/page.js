8f986abfe9f6cf7e719289a8a0dedc6526fd77ba
sandeep <s7895084627@gmail.com>
2024-12-24 01:44:42 +0530

    Added protected routes for admin dashboard

--- src/app/dashboards/admin-assigninstructor/page.js
+++ src/app/dashboards/admin-assigninstructor/page.js
@@ -1,29 +1,28 @@
+import ProtectedPage from "@/app/protectedRoutes";
 import AssignInst from "@/components/layout/main/dashboards/AssignInst";
 import DashboardContainer from "@/components/shared/containers/DashboardContainer";
 import HeadingDashboard from "@/components/shared/headings/HeadingDashboard";
 import ThemeController from "@/components/shared/others/ThemeController";
-import DsahboardWrapper from "@/components/shared/wrappers/DsahboardWrapper";
-import PageWrapper from "@/components/shared/wrappers/PageWrapper";
 export const metadata = {
   title: "Admin Reviews | Medh - Education LMS Template",
   description: "Admin Reviews | Medh - Education LMS Template",
 };
 const Admin_Reviews = () => {
   return (
-    // <PageWrapper>
-    <main>
-      {/* <DsahboardWrapper> */}
-      <DashboardContainer>
-        <div className="px-10">
-          <HeadingDashboard />
-        </div>
+    <ProtectedPage>
+      <main>
+        {/* <DsahboardWrapper> */}
+        <DashboardContainer>
+          <div className="px-10">
+            <HeadingDashboard />
+          </div>
 
-        <AssignInst />
-      </DashboardContainer>
-      {/* </DsahboardWrapper> */}
-      <ThemeController />
-    </main>
-    // </PageWrapper>
+          <AssignInst />
+        </DashboardContainer>
+        {/* </DsahboardWrapper> */}
+        <ThemeController />
+      </main>
+    </ProtectedPage>
   );
 };
 
--- src/app/dashboards/admin-profile/page.js
+++ src/app/dashboards/admin-profile/page.js
@@ -1,30 +1,26 @@
+import ProtectedPage from "@/app/protectedRoutes";
 import AdminProfileMain from "@/components/layout/main/dashboards/AdminProfileMain";
 import DashboardContainer from "@/components/shared/containers/DashboardContainer";
-import HeadingDashboard from "@/components/shared/headings/HeadingDashboard";
 import HeadingDashboardOnly from "@/components/shared/headings/HeadingDashbordsOnly";
 
 import ThemeController from "@/components/shared/others/ThemeController";
-import DsahboardWrapper from "@/components/shared/wrappers/DsahboardWrapper";
-import PageWrapper from "@/components/shared/wrappers/PageWrapper";
 export const metadata = {
   title: "Admin Profile | Medh - Education LMS Template",
   description: "Admin Profile | Medh - Education LMS Template",
 };
 const Admin_Profile = () => {
   return (
-    // <PageWrapper>
-    <main>
-      {/* <DsahboardWrapper> */}
-      <DashboardContainer>
-        <div className="px-4">
-          <HeadingDashboardOnly />
-        </div>
-        <AdminProfileMain />
-      </DashboardContainer>
-      {/* </DsahboardWrapper> */}
-      <ThemeController />
-    </main>
-    // </PageWrapper>
+    <ProtectedPage>
+      <main>
+        <DashboardContainer>
+          <div className="px-4">
+            <HeadingDashboardOnly />
+          </div>
+          <AdminProfileMain />
+        </DashboardContainer>
+        <ThemeController />
+      </main>
+    </ProtectedPage>
   );
 };
 
