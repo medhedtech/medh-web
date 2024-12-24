7b623ec0cdf6e3fa92afe2b225626b102a13e1cb
sandeep <s7895084627@gmail.com>
2024-12-24 16:17:06 +0530

    added small change on mobile view for navbar

--- src/components/layout/header/NavbarLogo.js
+++ src/components/layout/header/NavbarLogo.js
@@ -6,6 +6,7 @@ import NavbarMenu from "./NavbarMenu";
 import { Button } from "@mui/material";
 import ProgramsMenu from "./ProgramMenu";
 import { FaTimes } from "react-icons/fa";
+import { LogIn } from "lucide-react";
 
 const NavbarLogo = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -21,7 +22,15 @@ const NavbarLogo = () => {
 
   return (
     <div className="lg:col-start-1 lg:col-span-2">
-      <div className="flex items-center justify-center px-2">
+      <div className="bg-[#7ECA9D] w-full absolute top-0 left-0 z-20">
+        <Link
+          href="/login"
+          className="flex text-[12px] items-center justify-center w-full h-full font-normal text-[#FFFFFF] px-0 py-0"
+        >
+          <LogIn className="w-3 h-3 text-[#FFFFFF] mr-1" /> LOGIN
+        </Link>
+      </div>
+      <div className="flex items-center mt-4 justify-center px-2">
         <div className="flex items-center gap-6">
           {/* Menu Button */}
           <NavbarMenu menuOpen={menuOpen} toggleMenu={toggleMenu} />
