b42787633127d1c311a5a8e40f412526859c58ea
sandeep <s7895084627@gmail.com>
2024-12-27 14:26:40 +0530

    added all coorporate screen designs

--- src/app/dashboards/coorporate-enroll-new-courses/page.js
+++ src/app/dashboards/coorporate-enroll-new-courses/page.js
@@ -1,20 +1,20 @@
 import ProtectedPage from "@/app/protectedRoutes";
-import CorporateFreeCourses from "@/components/layout/main/dashboards/CorporateFreeCourses";
+import CoorporateNewCourses from "@/components/layout/main/dashboards/Coorporate-Enroll-New-Courses";
 import DashboardContainer from "@/components/shared/containers/DashboardContainer";
 import HeadingDashboard from "@/components/shared/headings/HeadingDashboard";
 import ThemeController from "@/components/shared/others/ThemeController";
 import React from "react";
 
-const Corporate_Free_Courses = () => {
+const Coorporate_Enroll_New_Courses = () => {
   return (
     <ProtectedPage>
       <DashboardContainer>
         <HeadingDashboard />
-        <CorporateFreeCourses />
+        <CoorporateNewCourses />
         <ThemeController />
       </DashboardContainer>
     </ProtectedPage>
   );
 };
 
-export default Corporate_Free_Courses;
+export default Coorporate_Enroll_New_Courses;
