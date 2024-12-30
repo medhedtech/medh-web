566a24c371d36f5b9ab04651fc2dde4a467743d2
sandeep <s7895084627@gmail.com>
2024-12-30 19:21:11 +0530

    small changes

--- src/app/dashboards/admin-management/page.js
+++ src/app/dashboards/admin-management/page.js
@@ -1,13 +1,13 @@
 import ProtectedPage from "@/app/protectedRoutes";
-import InstructoreManage from "@/components/layout/main/dashboards/InstructoreManage";
+import CoorporateAdminTable from "@/components/layout/main/dashboards/CoorporateAdmin_Table";
 import DashboardContainer from "@/components/shared/containers/DashboardContainer";
 import HeadingDashboard from "@/components/shared/headings/HeadingDashboard";
 import ThemeController from "@/components/shared/others/ThemeController";
 export const metadata = {
-  title: "Admin Reviews | Medh - Education LMS Template",
-  description: "Admin Reviews | Medh - Education LMS Template",
+  title: "Coorporate Admin Management | Medh - Education",
+  description: "Coorporate Admin Management | Medh - Education",
 };
-const Admin_Reviews = () => {
+const Admin_Coorporate_Management = () => {
   return (
     <ProtectedPage>
       <main>
@@ -15,7 +15,7 @@ const Admin_Reviews = () => {
           <div className="px-6">
             <HeadingDashboard />
           </div>
-          <InstructoreManage />
+          <CoorporateAdminTable />
         </DashboardContainer>
         <ThemeController />
       </main>
@@ -23,4 +23,4 @@ const Admin_Reviews = () => {
   );
 };
 
-export default Admin_Reviews;
+export default Admin_Coorporate_Management;
