28b42723e7e815fc17a9f596166cb82e00a3fce6
tapas-code <tapas.code247@gmail.com>
2024-12-30 19:45:27 +0530

    corporate dashboard fix

--- src/app/dashboards/coorporate-dashboard/page.js
+++ src/app/dashboards/coorporate-dashboard/page.js
@@ -1,17 +1,38 @@
+// import ProtectedPage from "@/app/protectedRoutes";
+// import CorporateDashboardMain from "@/components/layout/main/dashboards/CorporateDashboardMain";
+// import DashboardContainer from "@/components/shared/containers/DashboardContainer";
+// import ThemeController from "@/components/shared/others/ThemeController";
+// export const metadata = {
+//   title: "Corporate Dashboard | Medh - Education LMS Template",
+//   description: "Corporate Dashboard | Medh - Education LMS Template",
+// };
+// const Corporate_Dashboard = () => {
+//   return (
+//     <ProtectedPage>
+//       <main>
+//         <DashboardContainer>
+//           <CorporateDashboardMain />
+//         </DashboardContainer>
+//         <ThemeController />
+//       </main>
+//     </ProtectedPage>
+//   );
+// };
+
+// export default Corporate_Dashboard;
+
 import ProtectedPage from "@/app/protectedRoutes";
-import CorporateDashboardMain from "@/components/layout/main/dashboards/CorporateDashboardMain";
+import CoorporateMyCoursesDashboard from "@/components/layout/main/dashboards/CoorporateMyCourses_Dashboard";
 import DashboardContainer from "@/components/shared/containers/DashboardContainer";
 import ThemeController from "@/components/shared/others/ThemeController";
-export const metadata = {
-  title: "Corporate Dashboard | Medh - Education LMS Template",
-  description: "Corporate Dashboard | Medh - Education LMS Template",
-};
-const Corporate_Dashboard = () => {
+import React from "react";
+
+const Coorporate_Dashboard = () => {
   return (
     <ProtectedPage>
       <main>
         <DashboardContainer>
-          <CorporateDashboardMain />
+          <CoorporateMyCoursesDashboard />
         </DashboardContainer>
         <ThemeController />
       </main>
@@ -19,4 +40,5 @@ const Corporate_Dashboard = () => {
   );
 };
 
-export default Corporate_Dashboard;
+export default Coorporate_Dashboard;
+
