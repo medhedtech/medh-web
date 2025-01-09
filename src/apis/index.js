ca93cb698638f2531f0dcded0fa954b6f174f2ed
sandeep <s7895084627@gmail.com>
2025-01-09 14:09:42 +0530

    staging setup

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,5 @@
 // export const apiBaseUrl = "https://medh-backend.vercel.app/api/v1";  //vercel URL
-export const apiBaseUrl = "https://13.202.119.19.nip.io/api/v1"; //live instance URL
+export const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL //live instance URL
 // export const apiBaseUrl = "http://localhost:8080/api/v1"; // local URL
 
 export const apiUrls = {
