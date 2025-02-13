04585078cab126fe31d1c8a7a98152631a380fc1
abhijha903 <abhijha903@gmail.com>
2025-02-13 19:46:17 +0530

    #

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,4 +1,3 @@
-// export const apiBaseUrl = "https://medh-backend.vercel.app/api/v1";  //vercel URL
 export const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL; //live instance URL
 // export const apiBaseUrl = "http://localhost:8080/api/v1"; // local URL
 
