e018e765d3067bd40b9e3c41c2b9d44b8aed62c5
medhedtech <medhupskill@gmail.com>
2025-02-14 17:27:48 +0530

    #

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,5 @@
-export const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL; //live instance URL
-// export const apiBaseUrl = "http://localhost:8080/api/v1"; // local URL
+// export const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL; //live instance URL
+export const apiBaseUrl = "http://localhost:8080/api/v1"; // local URL
 
 export const apiUrls = {
   user: {
