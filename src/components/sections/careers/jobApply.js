c6bb1d2c68b5ec3d0e3fb535c1b22a9d55a93af1
medhedtech <medhupskill@gmail.com>
2025-02-14 11:12:18 +0530

    siteKey change

--- src/components/sections/careers/jobApply.js
+++ src/components/sections/careers/jobApply.js
@@ -253,7 +253,7 @@ function JobApply({ activeJob }) {
           </div>
 
           <ReCAPTCHA
-            sitekey="6Lf6ihYqAAAAAMNtP2TmPwnmZtxMBVHUKsL5JsS-"
+            sitekey="6LdHwxUqAAAAANjZ5-6I5-UYrL8owEGEi_QyJBX9"
             onChange={handleRecaptchaChange}
           />
           {/* ReCAPTCHA Error Message */}
