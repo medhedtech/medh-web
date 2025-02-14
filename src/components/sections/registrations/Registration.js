b22127ab704cbd0f050288281da6c4e64acd0687
medhedtech <medhupskill@gmail.com>
2025-02-14 11:08:50 +0530

    #

--- src/components/sections/registrations/Registration.js
+++ src/components/sections/registrations/Registration.js
@@ -327,7 +327,7 @@ const Registration = ({ showUploadField = false, pageTitle }) => {
                   )}
 
                   <ReCAPTCHA
-                    sitekey="6LeNH5QqAAAAAO98HJ00v5yuCkLgHYCSvUEpGhLb"
+                    sitekey="6Lf6ihYqAAAAAMNtP2TmPwnmZtxMBVHUKsL5JsS-"
                     onChange={handleRecaptchaChange}
                   />
                   {/* ReCAPTCHA Error Message */}
