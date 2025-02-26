be205123879416323c857e5c90b7f56084304013
Abhishek Jha <abhijha903@gmail.com>
2025-02-26 16:40:14 +0530

    ##

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import Home1 from "@/components/layout/main/Home1";
 import PageWrapper from "@/components/shared/wrappers/PageWrapper";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
@@ -7,6 +9,8 @@ export default function Home() {
       <main>
         <Home1/>
       </main>
+      <Analytics />
+      <SpeedInsights />
     </PageWrapper>
   );
 }
