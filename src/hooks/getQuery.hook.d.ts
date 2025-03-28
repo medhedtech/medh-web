68e319598dd3c5715e96fd317d9a5a63e8ea96f7
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 <abhijha903@gmail.com>
2025-03-28 13:37:29 +0530

    Add CORS management component, enhance API endpoints, and update development configurations

--- src/hooks/getQuery.hook.d.ts
+++ src/hooks/getQuery.hook.d.ts
@@ -1,17 +1,49 @@
-interface UseGetQueryParams<T> {
+import { AxiosRequestConfig, AxiosError } from 'axios';
+
+export interface RetryConfig {
+  maxRetries?: number;
+  retryDelay?: number | ((retryCount: number) => number);
+  retryableStatusCodes?: number[];
+  retryCondition?: (error: AxiosError) => boolean;
+}
+
+export interface UseGetQueryParams<T> {
   url: string;
-  config?: any; // e.g. AxiosRequestConfig from 'axios'
+  config?: AxiosRequestConfig;
   onSuccess?: (data: T) => void;
-  onFail?: (error: unknown) => void;
+  onFail?: (error: any) => void;
+  cacheKey?: string;
+  cacheTTL?: number;
+  skipCache?: boolean;
+  pagination?: {
+    pageParam: string;
+    limitParam: string;
+    page: number;
+    limit: number;
+  };
+  retry?: RetryConfig | boolean;
 }
 
-interface UseGetQueryReturn<T> {
+export interface UseGetQueryState<T> {
   data: T | null;
-  error: unknown;
+  error: Error | AxiosError | null;
   loading: boolean;
-  getQuery: (params: UseGetQueryParams<T>) => Promise<T | null>;
+  hasNextPage: boolean;
+  totalPages: number;
+  totalItems: number;
 }
 
-declare function useGetQuery<T = any>(): UseGetQueryReturn<T>;
+export interface UseGetQueryReturn<T> extends UseGetQueryState<T> {
+  getQuery: (params: UseGetQueryParams<T>) => Promise<T | null>;
+  refetch: () => Promise<T | null>;
+  cancelRequest: () => void;
+  clearCache: (cacheKey?: string) => void;
+  setData: React.Dispatch<React.SetStateAction<T | null>>;
+}
+
+declare function useGetQuery<T = any>(
+  initialUrl?: string, 
+  initialConfig?: AxiosRequestConfig
+): UseGetQueryReturn<T>;
 
 export default useGetQuery; 
