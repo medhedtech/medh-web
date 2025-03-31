d47c43b41517b4dd7f08821c24b7bf52e15cde71
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 <abhijha903@gmail.com>
2025-03-31 11:23:28 +0530

    Refactor integrated lessons layout: remove deprecated layout files, update layout components to TypeScript, and implement metadata for SEO

---
+++ src/app/instructors/[id]/page.tsx
@@ -0,0 +1,40 @@
+import InstructorDetailsMain from "@/components/layout/main/InstructorDetailsMain";
+import PageWrapper from "@/components/shared/wrappers/PageWrapper";
+import instructors from "@/../public/fakedata/instructors.json";
+import { notFound } from "next/navigation";
+import type { NextPage } from "next";
+
+export const metadata: { title: string; description: string } = {
+  title: "Instructor Details | Medh - Education LMS Template",
+  description: "Instructor Details | Medh - Education LMS Template",
+};
+
+interface InstructorDetailsProps {
+  params: {
+    id: string;
+  };
+}
+
+interface Instructor {
+  id: number;
+  // Add additional instructor properties if needed
+}
+
+const Instructor_Details: NextPage<InstructorDetailsProps> = ({ params }) => {
+  const { id } = params;
+  const isExistinstructor = (instructors as Instructor[])?.find(
+    ({ id: instructorId }) => instructorId === parseInt(id)
+  );
+  if (!isExistinstructor) {
+    notFound();
+  }
+  return (
+    <PageWrapper>
+      <main>
+        <InstructorDetailsMain id={id} />
+      </main>
+    </PageWrapper>
+  );
+};
+
+export default Instructor_Details; 
