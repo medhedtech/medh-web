8a357f00dd9a4bfd99f985ca18c157ecca0bcf55
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 <abhijha903@gmail.com>
2025-06-13 14:13:32 +0530

    refactor: update Quill editor styles by importing CSS in layout and removing from AddBlogs component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 import 'react-toastify/dist/ReactToastify.css';
 import '@/components/sidebar/sidebar-styles.css';
+import 'react-quill/dist/quill.snow.css';
 import ThemeController from '@/components/shared/others/ThemeController';
 import GoogleAnalytics from '@/components/shared/analytics/GoogleAnalytics';
 import { Suspense } from 'react';
