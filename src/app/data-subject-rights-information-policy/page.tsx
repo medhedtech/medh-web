41be5fb8c7759a2668ac068009b55bbbb172e7c1
medhedtech <medhupskill@gmail.com>
2025-08-09 20:43:00 +0530

    Improve the home page

---
+++ src/app/data-subject-rights-information-policy/page.tsx
@@ -0,0 +1,20 @@
+import DataSubjectRightsPolicy from "@/components/layout/main/DataSubjectRightsPolicy";
+import PageWrapper from "@/components/shared/wrappers/PageWrapper";
+import React from "react";
+
+export const metadata = {
+  title: "Data Subject Rights Information Policy",
+  description: "Data Subject Rights Information Policy | Medh",
+};
+
+const DataSubjectRightsPage = () => {
+  return (
+    <PageWrapper>
+      <main className="pt-16">
+        <DataSubjectRightsPolicy />
+      </main>
+    </PageWrapper>
+  );
+};
+
+export default DataSubjectRightsPage;
